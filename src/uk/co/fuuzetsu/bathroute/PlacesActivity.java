5619167f0b467ea4820d71efb1b4393003beef42
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2013-12-08 03:47:04 +0000

    Tab activity stubs.

---
+++ src/uk/co/fuuzetsu/bathroute/PlacesActivity.java
@@ -0,0 +1,23 @@
+package uk.co.fuuzetsu.bathroute;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.TextView;
+
+public class PlacesActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView  tv=new TextView(this);
+        tv.setTextSize(25);
+        tv.setGravity(Gravity.CENTER_VERTICAL);
+        tv.setText("This is a PlacesActivity.");
+
+        setContentView(tv);
+    }
+
+
+}
