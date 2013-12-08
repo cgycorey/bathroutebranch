b13b8f0fe239db9ab2b340e1763792e813632e0b
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2013-12-08 05:30:27 +0000

    Places elements

--- src/uk/co/fuuzetsu/bathroute/PlacesActivity.java
+++ src/uk/co/fuuzetsu/bathroute/PlacesActivity.java
@@ -1,23 +1,25 @@
 package uk.co.fuuzetsu.bathroute;
 
 import android.app.Activity;
-import android.graphics.Typeface;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class PlacesActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.places);
 
-        TextView tv = new TextView(this);
-        tv.setTextSize(25);
-        tv.setGravity(Gravity.TOP);
-        tv.setText("Places");
+        final ListView lv = (ListView) findViewById(R.id.places_list_view);
 
-        setContentView(tv);
+        String[] values = new String[] { "3WN", "Library", "3E", "East Building" };
+
+        ArrayAdapter<String> lvadapter
+            = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, values);
+
+        lv.setAdapter(lvadapter);
     }
 
 
