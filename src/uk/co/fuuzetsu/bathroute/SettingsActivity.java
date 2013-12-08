58d66b9f05d0d3c980c2b967c780886e433ca375
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2013-12-08 05:14:49 +0000

    Create a dummy list for settings tab

--- src/uk/co/fuuzetsu/bathroute/SettingsActivity.java
+++ src/uk/co/fuuzetsu/bathroute/SettingsActivity.java
@@ -3,21 +3,26 @@ package uk.co.fuuzetsu.bathroute;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Gravity;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
 
 public class SettingsActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
 
-        TextView tv = new TextView(this);
-        tv.setTextSize(25);
-        tv.setGravity(Gravity.TOP);
-        tv.setText("Settings");
+        final ListView lv = (ListView) findViewById(R.id.settings_list_view);
 
-        setContentView(tv);
+        String[] values = new String[] { "Image", "Favourites", "Position broadcasting",
+                                         "Map", "Storage", "About" };
+
+        ArrayAdapter<String> lvadapter
+            = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, values);
+
+        lv.setAdapter(lvadapter);
     }
-
-
 }
