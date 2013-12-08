c4e39ae1204a976feac638dcd0565aac6ca62a2b
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2013-12-08 05:25:32 +0000

    Basic Misc elements

--- src/uk/co/fuuzetsu/bathroute/MiscActivity.java
+++ src/uk/co/fuuzetsu/bathroute/MiscActivity.java
@@ -2,21 +2,24 @@ package uk.co.fuuzetsu.bathroute;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MiscActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.misc);
 
-        TextView tv = new TextView(this);
-        tv.setTextSize(25);
-        tv.setGravity(Gravity.TOP);
-        tv.setText("Misc");
+        final ListView lv = (ListView) findViewById(R.id.misc_list_view);
 
-        setContentView(tv);
+        String[] values = new String[] { "Friends", "Computers", "Mailroom", "Toilets" };
+
+        ArrayAdapter<String> lvadapter
+            = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, values);
+
+        lv.setAdapter(lvadapter);
     }
 
 
